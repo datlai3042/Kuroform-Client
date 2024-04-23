d1694fb5b699928ec0f09d86358e41d12b74cc79
ngayMaivanden <datlai304@gmail.com>
2024-04-23 15:15:01 +0700

    đổi biến môi trường

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	env: {
+		BACK_END_URL: NEXT_PUBLIC_BACK_END_URL,
+		CLIENT_END_URL: NEXT_PUBLIC_CLIENT_URL,
+	},
 	// reactStrictMode: false,
 };
 
