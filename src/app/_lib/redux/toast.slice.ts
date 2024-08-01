d131efd2fda7f40289913351b099c5ef0fefdb90
datlai3042 <datlai304@gmail.com>
2024-08-01 22:43:26 +0700

    Giảm thời gian toast

--- src/app/_lib/redux/toast.slice.ts
+++ src/app/_lib/redux/toast.slice.ts
@@ -11,7 +11,7 @@ type InitialState = {
 const initialState: InitialState = {
       toast_stack: [],
       toast_queue: [],
-      toast_timer: 100,
+      toast_timer: 10,
       toast_max_show: 4,
 };
 
